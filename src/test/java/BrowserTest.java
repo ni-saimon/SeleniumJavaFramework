9143bbf00bd5e55e8e586ca8d9452c7dcdac8eee
ni-saimon <nafizimtiaz314@gmail.com>
2022-01-25 07:31:54 +0600

    Initial commit

---
+++ src/test/java/BrowserTest.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://seleniumhq.org/");
+    }
+}
